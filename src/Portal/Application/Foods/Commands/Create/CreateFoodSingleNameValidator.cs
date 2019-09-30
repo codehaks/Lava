1a66261748693702c243ff09536cb66ed8a29ab6
Codehaks <ghods.hakim@gmail.com>
2019-09-30 15:30:38 +0330

    save

--- src/Portal/Application/Foods/Commands/Create/CreateFoodSingleNameValidator.cs
+++ src/Portal/Application/Foods/Commands/Create/CreateFoodSingleNameValidator.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using MediatR;
+using Portal.Persistance;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Portal.Application.Foods.Commands.Create
 {
-    class CreateFoodSingleNameValidator:CommandValidator<CreateFoodCommand>
+    public class CreateFoodSingleNameValidator<CreateFoodCommand, CreateFoodCommandResult> :
+        IPipelineBehavior<CreateFoodCommand, CreateFoodCommandResult>
     {
+        private readonly PortalDbContext _db;
+
+        public CreateFoodSingleNameValidator(PortalDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<CreateFoodCommandResult> Handle(CreateFoodCommand request, CancellationToken cancellationToken, RequestHandlerDelegate<CreateFoodCommandResult> next)
+        {
+            var r = (CreateFoodCommand)request;
+            
+            //_db.Foods.Where(f=>f.Name.Trim()==request.)
+            await Task.CompletedTask;
+            return next();
+        }
     }
 }
