eacd7c087c8808573d171c7278127bde096a758b
Codehaks <ghods.hakim@gmail.com>
2019-10-25 03:15:24 +0330

    Events

---
+++ src/Portal/Domain/Model/OrderAggregate/Events/OrderDeliveredEvent.cs
@@ -0,0 +1,23 @@
+﻿using Portal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Domain.Model.OrderAggregate.Events
+{
+    class OrderDeliveredEvent
+    {
+        public OrderDeliveredEvent(Order order)
+        {
+            Order = order;
+        }
+        public Order Order { get;private set; }
+    }
+
+    class OrderDeliveryEvents
+    {
+        public delegate void OrderDeliveredEventHandler(OrderDeliveredEvent e);
+        public event OrderDeliveredEventHandler OrderDeliveredEvent;
+
+    }
+}
