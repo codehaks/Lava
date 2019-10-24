ce4d6f055b673d28a4022d8cdf25753cef86b1a9
Codehaks <ghods.hakim@gmail.com>
2019-10-24 13:45:48 +0330

    Notification

---
+++ src/Portal/Application/OrderApplication/Notifications/SendEmailOrderCreatedNotificationHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Portal.Application.OrderApplication.Notifications
+{
+    class OrderCreatedNotificationHandler : INotificationHandler<OrderCreatedNotification>
+    {
+        public Task Handle(OrderCreatedNotification notification, CancellationToken cancellationToken)
+        {
+            // Send email to user 
+            return Task.CompletedTask;
+        }
+    }
+}
